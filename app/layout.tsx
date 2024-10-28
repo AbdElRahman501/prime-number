a88e718827832e80076b94d7a46fa52c38cf07dd
AbdElRahman501 <abdelrahman.6182@gmail.com>
2024-10-28 22:32:42 +0300

    add analytics and speed insights and google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import dynamic from "next/dynamic";
 import Header from "@/components/Header";
 import { NextAuthProvider } from "@/NextAuthProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 // Load fonts with selected weights and subsets
 const rubik = Rubik({
@@ -37,6 +40,19 @@ export default function RootLayout({
       dir="rtl"
       className={`${rubik.className} ${inter.variable}`} // Combining Rubik and Inter fonts
     >
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P759KG7P');`,
+          }}
+        ></Script>
+      </head>
       <body className="antialiased">
         {/* Header Component */}
         <NextAuthProvider>
@@ -48,6 +64,17 @@ export default function RootLayout({
 
         {/* Footer Component */}
         <Footer />
+        <SpeedInsights />
+        <Analytics />
+
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P759KG7P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
