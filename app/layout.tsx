3ca87feeeeca318826f934566d6c697c2a5ec767
AbdElRahman501 <abdelrahman.6182@gmail.com>
2024-10-28 23:43:14 +0300

    add GoogleTagManager

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Header from "@/components/Header";
 import { NextAuthProvider } from "@/NextAuthProvider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 // Load fonts with selected weights and subsets
 const rubik = Rubik({
@@ -40,19 +40,7 @@ export default function RootLayout({
       dir="rtl"
       className={`${rubik.className} ${inter.variable}`} // Combining Rubik and Inter fonts
     >
-      <head>
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P759KG7P');`,
-          }}
-        ></Script>
-      </head>
+      <GoogleTagManager gtmId="GTM-P759KG7P" />
       <body className="antialiased">
         {/* Header Component */}
         <NextAuthProvider>
@@ -66,15 +54,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <Footer />
         <SpeedInsights />
         <Analytics />
-
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P759KG7P"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
       </body>
     </html>
   );
